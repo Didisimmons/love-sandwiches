b801cc999918b77307fffa02681d121ceb902542
Didisimmons <onyema.onyejekwe@gmail.com>
2021-11-25 22:25:37 +0000

    add calculated stock numbers for each sandwich type

--- run.py
+++ run.py
@@ -85,6 +85,16 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    print("Make the following numbers of sandwiches for next market:\n")
+    
+    return {heading: data for heading, data in zip(headings, data)}
+    
 
 def get_last_5_entries_sales():
     """
@@ -128,7 +138,10 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
+print("Welcome to Love Sandwiches data automation.\n")
+stock_data = main()
+
